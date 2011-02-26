55832c4ef0377c86ba21bde70450505e365a10c0
Errin Larsen <elarsen@insightcards.com>
2011-02-25 19:42:44 -0600

    [0.1.1] Add code to use the session to store the state of the Pickers deck

--- controllers/root.rb
+++ controllers/root.rb
@@ -1,3 +1,10 @@
 get '/' do
+  if session[:pickers].empty? || session[:pickers].nil?
+    pickers =  Picker.all.shuffle
+  else
+    pickers = session[:pickers]
+  end
+  @picker = pickers.shift
+  session[:pickers] = pickers
   haml :index
 end
